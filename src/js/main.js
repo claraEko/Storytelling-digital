d295db2104bcc1f2d5ee3b3f86e4ee925bf5a9ff
claradelvecchio <clara.anquetil@studentfr.ch>
2025-11-12 17:02:16 +0100

    end of day

--- src/js/main.js
+++ src/js/main.js
@@ -21,45 +21,84 @@ tl.to(".first-button", {
   opacity: 0,
   ease: "none",
 })
-  .to(
-    ".zoom-1-a",
-    {
-      scale: 1.5,
-      y: "50%",
-      duration: 5,
-      filter: "blur(5px)",
-      ease: "none",
-    },
-    "-=0.5"
-  )
-  .to(
-    ".zoom-1-b",
-    {
-      scale: 3,
-      y: "100%",
-      filter: "blur(5px)",
-      duration: 6,
-      ease: "none",
-    },
-    "-=2"
-  )
-  .to(
-    ".zoom-1-text",
-    {
-      scale: 2,
-      y: "-200%",
-      duration: 3,
-      ease: "none",
-    },
-    "-=5.5"
-  )
-  .to(
-    ".zoom-1-background",
-    {
-      scale: 2,
-      opacity: 0,
-      duration: 1,
-      ease: "none",
-    },
-    "-=3"
-  );
+  .to(".zoom-1-a", {
+    scale: 1.5,
+    y: "50%",
+    duration: 5,
+    filter: "blur(5px)",
+    ease: "none",
+  })
+  .to(".zoom-1-b", {
+    scale: 3,
+    y: "100%",
+    filter: "blur(5px)",
+    duration: 5,
+    ease: "none",
+  })
+  .to(".zoom-1-title", {
+    scale: 2,
+    y: "-100%",
+    duration: 5,
+    opacity: 0,
+    ease: "none",
+  })
+  .to(".zoom-1-background", {
+    scale: 2,
+    opacity: 0,
+    duration: 3,
+    ease: "none",
+  })
+  .to(".zoom-2-a", {
+    scale: 1.5,
+    y: "70%",
+    duration: 5,
+    filter: "blur(5px)",
+    ease: "none",
+  })
+  .to(".zoom-2-b", {
+    scale: 3,
+    y: "100%",
+    filter: "blur(5px)",
+    duration: 5,
+    ease: "none",
+  })
+  .to(".zoom-2-text", {
+    scale: 2,
+    x: "100%",
+    duration: 5,
+    opacity: 0,
+    ease: "none",
+  })
+  .to(".zoom-2-background", {
+    scale: 2,
+    opacity: 0,
+    duration: 3,
+    ease: "none",
+  })
+  .to(".zoom-3-a", {
+    scale: 1.5,
+    y: "70%",
+    duration: 5,
+    filter: "blur(5px)",
+    ease: "none",
+  })
+  .to(".zoom-3-b", {
+    scale: 3,
+    y: "100%",
+    filter: "blur(5px)",
+    duration: 5,
+    ease: "none",
+  })
+  .to(".zoom-3-text", {
+    scale: 2,
+    x: "100%",
+    duration: 5,
+    opacity: 0,
+    ease: "none",
+  })
+  .to(".zoom-3-background", {
+    scale: 2,
+    opacity: 0,
+    duration: 3,
+    ease: "none",
+  });
